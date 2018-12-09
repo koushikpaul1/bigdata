d0b25bed1046dac49b9acaa2f1d30204c8119720
koushik <koushik@ubuntu>
2018-12-09 20:58:39 +0530

    5

--- MapReduce/src/main/java/com/edge/basic/WordCount.java
+++ MapReduce/src/main/java/com/edge/basic/WordCount.java
@@ -13,38 +13,33 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.log4j.PropertyConfigurator;
 
 public class WordCount {
-    public static void main(String[] args) throws Exception { 
-    	
-    	String log4jConfPath = "log4j.properties";
+	public static void main(String[] args) throws Exception {
+
+		String log4jConfPath = "log4j.properties";
 		PropertyConfigurator.configure(log4jConfPath);
-    	Configuration conf = new Configuration();
+		Configuration conf = new Configuration();
 		if (args.length != 2) {
 			System.err.println("Usage: wordcount <in> <out>");
 			System.exit(2);
 		}
 		Job job = Job.getInstance(conf, "word count");
-		
+
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(Map.class);
 		job.setCombinerClass(Reduce.class);
 		job.setReducerClass(Reduce.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
-		
+
 		Path outputPath = new Path(args[1]);
 		FileSystem fs = FileSystem.get(new URI(outputPath.toString()), conf);
-		fs.delete(outputPath,true);
-		
-		
+		fs.delete(outputPath, true);
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, outputPath);
-		System.out.println("Number of map tasks "+conf.get("mapred.map.tasks"));
+		System.out.println("Number of map tasks " + conf.get("mapred.map.tasks"));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
-    
+	}
+
 }
-
-
-
