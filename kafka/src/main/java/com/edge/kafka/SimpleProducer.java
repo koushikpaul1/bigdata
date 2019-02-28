40208bacd59a42146e7877677906ecdbdbd09379
koushikpaul1 <koushikpaul1@hmail.com>
2019-02-28 12:41:02 -0800

    kafka SynchronousProducer

--- kafka/src/main/java/com/edge/kafka/SimpleProducer.java
+++ kafka/src/main/java/com/edge/kafka/SimpleProducer.java
@@ -6,19 +6,19 @@ public class SimpleProducer {
   
    public static void main(String[] args) throws Exception{
            
-      String topicName = "SimpleProducerTopic";
+      String topicName = "rpltpc";
 	  String key = "Key1";
 	  String value = "Value-1";
       
       Properties props = new Properties();
-      props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+      props.put("bootstrap.servers", "localhost:9092,localhost:9093,localhost:9094");
       props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");         
       props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 	        
       Producer<String, String> producer = new KafkaProducer <>(props);
 	
 	  ProducerRecord<String, String> record = new ProducerRecord<>(topicName,key,value);
-	  producer.send(record);	       
+	  System.out.println(producer.send(record));	       
       producer.close();
 	  
 	  System.out.println("SimpleProducer Completed.");
